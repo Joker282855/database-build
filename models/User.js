60e997c91b6f1a77089fa434b9c42fd51d5f4dd8
unknown <jtjackson325@gmail.com>
2021-08-28 14:02:26 -0600

    got the file setup where the serve connects innitially in the computer

---
+++ models/User.js
@@ -0,0 +1,18 @@
+// const { Schema, model } = require('mongoose');
+
+// const UserSchema = new Schema({
+    // username: {
+        // type: String,
+        // unique: true,
+        // required: [true, 'Please enter a username'],
+        // trim: true
+    // },
+    // email: {
+        // type: String,
+        // required: [true, 'Please enter a valid email'],
+        // unique: true,
+        // Validate: {
+            //validator: () => Promise.resolve(false),
+        // }
+    //}
+//})
