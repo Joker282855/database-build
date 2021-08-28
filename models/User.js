6f9b77e7a88d7a913005e04bb569451936a1a2f5
unknown <jtjackson325@gmail.com>
2021-08-28 15:21:29 -0600

    got the connection for the mongoose database set up

--- models/User.js
+++ models/User.js
@@ -1,18 +1,22 @@
-// const { Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-// const UserSchema = new Schema({
-    // username: {
-        // type: String,
-        // unique: true,
-        // required: [true, 'Please enter a username'],
-        // trim: true
-    // },
-    // email: {
-        // type: String,
-        // required: [true, 'Please enter a valid email'],
-        // unique: true,
-        // Validate: {
-            //validator: () => Promise.resolve(false),
-        // }
-    //}
-//})
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: [true, 'Please enter a username'],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: true,
+        match: [/^([a-z0-9_\.-]+)@([\wa-z\.-]+)\.([a-z\.]{2,6})$/, "Please enter a valid email address"]
+    },
+    thoughts: [],
+    friends: []
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express =require('express');
 
 const app = express();
@@ -6,6 +7,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODDB_URI || 'mongodb://localhost:/database-build', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`You are connected to:${PORT}`));
